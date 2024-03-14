bf798f16ad358b82d1285ed5fd3543d53bdd2023
Светлана Дябло <svetlana.dyablo@Svetlanas-MacBook-Air.local>
2024-03-14 17:47:58 +0300

    fix start code

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import './App.css';
-import React from 'react';
-import cat from '../src/assets/images/cat-1.jpg'
-import cat2 from '../src/assets/images/cat-2.jpg'
+import image from './assets/images/image.jpg';
+
 
 
 function App() {
     return (
         <div className="App">
             <p>Hello, Retina &#128075;</p>
-
-            {/*<div className={'bg'}></div>*/}
-            {/*<img src={cat2} alt="cat"/>*/}
-
+            {/* <div className={'bg'}></div> */}
+            {/* <img src={image} alt="nature"/> */}
         </div>
     );
 }
